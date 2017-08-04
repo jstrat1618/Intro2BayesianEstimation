580692df98f0e17b0c73c5deffffc648685e6cdb
Justin <jstrat1.618@gmail.com>
2017-08-04 11:01:05 -0600

    Fix label on posterior distribution plot

--- Bern_Beta.py
+++ Bern_Beta.py
@@ -18,20 +18,22 @@ posterior_alpha = prior_alpha + x.sum()
 posterior_beta = prior_beta + n - x.sum()
 
 xgrid = np.arange(-0.1,1.1,0.01)
+#Evaluate the prior over the grid
 prior_of_xgrid = beta.pdf(xgrid, a = prior_alpha, b = prior_beta)
 
-#Plot the prior distributiont
+#Plot the prior distribution
 plt.plot(xgrid, prior_of_xgrid)
 plt.xlabel(r'$\theta$')
 plt.ylabel(r'$\pi(\theta)$')
 plt.title("Prior Distribtuion")
 plt.show()
 
-
+#Evaluate the postior over the grid
 posterior_of_xgrid = beta.pdf(xgrid, a = posterior_alpha, b = posterior_beta)
+#Plot the posterior
 plt.plot(xgrid, posterior_of_xgrid)
-plt.xlabel("x")
-plt.ylabel("posterior")
+plt.xlabel(r'$\theta$')
+plt.ylabel(r'$\pi(\theta | x)$')
 plt.title("Posterior Distribtuion")
 plt.show()
 
