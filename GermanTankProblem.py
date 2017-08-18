0813f266f6fb56b11711ab84cda9f285f85a418d
Justin <jstrat1.618@gmail.com>
2017-08-18 08:44:18 -0600

    Add stem plot

--- GermanTankProblem.py
+++ GermanTankProblem.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.misc import comb
 
-m = 105
+m = 64
 k = 5
 def eval_point(n):
     if n <m:
@@ -10,32 +10,41 @@ def eval_point(n):
     else:
         return((k-1)/k *comb(m-1,k-1)/comb(n, k))
 
-def posterior(N):
+def posterior(n):
     list_eval_points = []
-    for n in N:
-        eval = eval_point(n)
+    for m in n:
+        eval = eval_point(m)
         list_eval_points.append(eval)
     out = np.array(list_eval_points)
     return(out)    
 
-def prior(x):
-    diff = x.max() - x.min()
-    out = np.repeat(1/diff, len(x))
+def prior(n):
+    diff = n.max() - n.min()
+    out = np.repeat(1/diff, len(n))
     return(out)
     
-x = np.array(range(1,501))
+N = np.array(range(1,201))
 
-plt.plot(x,prior(x))
+plt.plot(N,prior(N))
 plt.xlabel("N")
 plt.ylabel(r'$\pi(N)$')
 plt.title("Prior Distribtuion")
 plt.show()
 
+fig, ax = plt.subplots()
+ax.stem(N, prior(N))
+plt.show()
 
-plt.plot(x,posterior(x))
+plt.plot(N,posterior(N))
 plt.xlabel("N")
 plt.ylabel(r'$\pi(N | x)$')
 plt.title("Posterior Distribtuion")
 plt.show()
+
+
+fig, ax = plt.subplots()
+ax.stem(N, posterior(N))
+plt.show()
+
 
 
