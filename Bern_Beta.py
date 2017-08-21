ea33760650466da2a89ccfcb17095ebf59cbdf3c
Justin <jstrat1.618@gmail.com>
2017-08-21 14:45:28 -0600

    Reorient Bern_Beta to flow with presentation

--- Bern_Beta.py
+++ Bern_Beta.py
@@ -1,19 +1,12 @@
 import numpy as np
-
-#Define parameter and sample size
-theta , n = 0.5, 10
-
-#Generate Data
-x = np.random.binomial(1, theta, n)
+import matplotlib.pyplot as plt
+from scipy.stats import beta
 
 #Define hyperparameters
 prior_alpha, prior_beta = 1,1
 
 
-import matplotlib.pyplot as plt
-from scipy.stats import beta
-
-xgrid = np.arange(-0.1,1.1,0.01)
+xgrid = np.arange(0,1,0.01)
 #Evaluate the prior over the grid
 prior_of_xgrid = beta.pdf(xgrid, a = prior_alpha, b = prior_beta)
 
@@ -25,19 +18,28 @@ plt.ylabel(r'$\pi(\theta)$')
 plt.title("Prior Distribtuion")
 plt.show()
 
+
+#Define parameter and sample size
+theta , n = 0.5, 10
+
+#Generate Data
+x = np.random.binomial(1, theta, n)
+y = x.sum()
+
+
 #Define the Likelihood
 def Lhood_bern(theta):
-    out = theta**x.sum() *(1-theta)**(len(x) - x.sum())
+    out = theta**y *(1-theta)**(n - y)
     return(out)
+
 
 #Plot the Likelihood
 plt.plot(xgrid, Lhood_bern(xgrid))
+plt.axvline(x.mean(), color='orange')
 plt.xlabel(r'$\theta$')
 plt.ylabel(r'$L(\theta | x)$')
 plt.title("Likelihood")
-plt.show()    
-
-
+plt.show() 
     
 #Define Posterior hyperparameters
 posterior_alpha = prior_alpha + x.sum()
